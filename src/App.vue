4bc09ac8a9031f0472b623cc5f482857a5162a0c
Poisdim <Poisdim@mail.ru>
2021-10-16 19:06:11 +0300

    start project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="">
+        Hello
+    </div>
+</template>
+
+<script>
+    export default {
+        name: '',
+        props: {},
+        data() {
+            return {}
+        },
+        computed: {},
+        methods: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    devServer: {
+        open: true
+    },
+    css: {
+        loaderOptions: {
+            sass: {
+                prependData: `@import "@/assets/styles/styles.scss";`
+            }
+        }
+    }
+};
